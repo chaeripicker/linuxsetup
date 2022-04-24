5253752bb8d9e1b2320ecd57542658339679019c
chaeripicker <94459015+chaeripicker@users.noreply.github.com>
2022-04-24 15:17:06 +0900

    dotnet 너무빨리열려한거 수정

--- ssb3.sh
+++ ssb3.sh
@@ -2,11 +2,11 @@
 cd ~/Desktop
 alias chrome="/c/'Program Files'/Google/Chrome/Application/chrome.exe"
 chrome "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive"
-mv ~/Downloads/VSCode-win32-x64-1.66.2.zip ~/Desktop/VSCode-win32-x64-1.66.2.zip
-./dotnet-sdk-6.0.202-win-x64.exe &
 chrome "https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip"
 curl -O https://download.visualstudio.microsoft.com/download/pr/e4f4bbac-5660-45a9-8316-0ffc10765179/8ade57de09ce7f12d6411ed664f74eca/dotnet-sdk-6.0.202-win-x64.exe
 curl -O https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+mv ~/Downloads/VSCode-win32-x64-1.66.2.zip ~/Desktop/VSCode-win32-x64-1.66.2.zip
+./dotnet-sdk-6.0.202-win-x64.exe &
 git clone https://github.com/chaeripicker/TheDarkDungeons.git ~/TheDarkDungeons &
 unzip VSCode-win32-x64-1.66.2.zip -d code
 mv ~/Downloads/Fira_Code_v6.2.zip ~/Desktop/Fira_Code_v6.2.zip
