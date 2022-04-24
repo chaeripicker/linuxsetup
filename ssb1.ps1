d1ce75530281c82824655b5e575778bfe87fdd0d
chaeripicker <94459015+chaeripicker@users.noreply.github.com>
2022-04-24 09:30:30 +0900

    Update ssb1.ps1

--- ssb1.ps1
+++ ssb1.ps1
@@ -1,5 +1,5 @@
-# Set-ExecutionPolicy Bypass
-# winget
-# .\gitsetup.ps1
-winget install --id Git.Git -e --source winget
-#Start-Process 'C:\Program Files\Git\git-bash.exe' C:\Users\CKIRUser\Downloads\linuxsetup-main\linuxsetup-main\ssbinit.sh
+winget; winget install --id Git.Git -e --source winget;
+New-Item C:\Users\CKIRUser\.bashrc -Value "git clone https://github.com/chaeripicker/linuxsetup.git";
+New-Item C:\Users\CKIRUser\.bash_profile -Value "test -f ~/.profile && . ~/.profile
+test -f ~/.bashrc && . ~/.bashrc";
+start "C:\Program Files\Git\bin\bash.exe" -WorkingDirectory 'C:\Users\CKIRUser\Desktop'
