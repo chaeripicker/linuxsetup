754a431f7b7d291259b83f8f2cba874608c66a1a
chaeripicker <94459015+chaeripicker@users.noreply.github.com>
2022-04-24 09:34:37 +0900

    Update ssb1.ps1

--- ssb1.ps1
+++ ssb1.ps1
@@ -2,4 +2,5 @@ winget; winget install --id Git.Git -e --source winget;
 New-Item C:\Users\CKIRUser\.bashrc -Value "git clone https://github.com/chaeripicker/linuxsetup.git";
 New-Item C:\Users\CKIRUser\.bash_profile -Value "test -f ~/.profile && . ~/.profile
 test -f ~/.bashrc && . ~/.bashrc";
-start "C:\Program Files\Git\bin\bash.exe" -WorkingDirectory 'C:\Users\CKIRUser\Desktop'
+start "C:\Program Files\Git\bin\bash.exe" -WorkingDirectory 'C:\Users\CKIRUser\Desktop';
+Start-Process 'C:\Program Files\Git\git-bash.exe' C:\Users\CKIRUser\Desktop\linuxsetup\ssb3.sh
